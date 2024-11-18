52817d611c978360fb087b0d7dc09000d8ce43d6
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2024-11-18 18:33:32 +0100

    Hello World

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def homePageView(request):
+    return HttpResponse("<h1>Hello World</h1>")
 
 # Create your views here.
